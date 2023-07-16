afda9a87b253e2eb6a1ac86faa1546acf23c2952
SunLxy <1011771396@qq.com>
2023-07-16 08:39:28 +0800

    chore:升级版本

--- .saqurc.ts
+++ .saqurc.ts
@@ -4,14 +4,17 @@ import autoCreateEnter from "@saqu/auto-create-enter"
 
 export default defineConfig({
   entry: '!src/.cache/main.jsx',
-  plugins: [new autoCreateRoutes({
-    rootRoutes: "@/routes",
-    presetsImport: `import Preview from "@/components/Preview"`,
-    fileExt: "md",// 直接加载 md 文件
-    renderConfig: ({ pathName, oFilePath }) => {
-      return { configStr: `\t{ path:"${pathName}",element:<Preview path={()=>import("${oFilePath}")} /> },\n` }
-    }
-  }), new autoCreateEnter()],
+  plugins: [
+    new autoCreateRoutes({
+      rootRoutes: "@/routes",
+      presetsImport: `import Preview from "@/components/Preview";`,
+      fileExt: "md",// 直接加载 md 文件
+      renderConfig: ({ pathName, oFilePath }) => {
+        return { configStr: `\t{ path:"${pathName}",element:<Preview path={()=>import("${oFilePath}")} /> },\n` }
+      }
+    }),
+    new autoCreateEnter()
+  ],
   module: {
     rules: [
       {
